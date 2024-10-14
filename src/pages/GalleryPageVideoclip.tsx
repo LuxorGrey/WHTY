ab5f81de92966d93bfc759c4a2d9e95cb494d55c
LuxorGrey <luxorgrey@gmail.com>
2024-10-14 02:42:40 +0200

    fix name

--- src/pages/GalleryPageVideoclip.tsx
+++ src/pages/GalleryPageVideoclip.tsx
@@ -1,9 +1,9 @@
 import GalleryContent from "../components/GalleryContent";
-import image1 from "../assets/image/Logos/logo1.jpg";
-import image2 from "../assets/image/Logos/logo2.jpg";
-import image3 from "../assets/image/Logos/logo3.jpg";
-import image4 from "../assets/image/Logos/logo4.jpg";
-import image5 from "../assets/image/Logos/logo5.jpg";
+import image1 from "../assets/image/logos/logo1.jpg";
+import image2 from "../assets/image/logos/logo2.jpg";
+import image3 from "../assets/image/logos/logo3.jpg";
+import image4 from "../assets/image/logos/logo4.jpg";
+import image5 from "../assets/image/logos/logo5.jpg";
 //
 import background from "../assets/image/Headers/header2.jpg";
 
